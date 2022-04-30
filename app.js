7d9ab4363625b8bb33022e01a66e2b2b4783dfd0
harsh <63902836+harshmodi227@users.noreply.github.com>
2022-04-30 23:41:38 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const crudRoutes = require('./routes/Crudperations');
+
+const mongoose = require("mongoose");
+const url = "mongodb://localhost/HarshModi";
+
+mongoose.connect(url);
+
+const conn = mongoose.connection;
+
+app.use(express.json());
+conn.on('open', ()=>{
+    console.log("connected to db")
+})
+app.use('/harsh',crudRoutes);
+
+app.listen(6000, ()=>{
+    console.log('server is on port 6000');
+});
